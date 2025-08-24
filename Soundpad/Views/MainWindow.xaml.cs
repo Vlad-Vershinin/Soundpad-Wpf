fbb0bdcf92f27c50c8b1b41d3904b4dc22a8f9ef
Vlad-Vershinin <vladvesinin676@gmail.com>
2025-08-24 23:40:48 +0500

    create menu

---
+++ Soundpad/Views/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace Soundpad.Views;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void MenuItem_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+}
