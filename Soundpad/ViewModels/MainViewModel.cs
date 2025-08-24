fb48bdada28d94db51f6e99575a954c19c2baee6
Vlad-Vershinin <vladversinin676@gmail.com>
2025-08-25 00:17:16 +0500

    crate MainViewModel and Open Github btn

---
+++ Soundpad/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using ReactiveUI;
+using System.Diagnostics;
+using System.Reactive;
+
+namespace Soundpad.ViewModels;
+
+public class MainViewModel : ReactiveObject
+{
+    public ReactiveCommand<string, Unit> OpenUrlCommand { get; }
+
+    public MainViewModel()
+    {
+        OpenUrlCommand = ReactiveCommand.Create<string>(OpenUrl);
+    }
+
+    private void OpenUrl(string url)
+    {
+        try
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = url,
+                UseShellExecute = true
+            });
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
